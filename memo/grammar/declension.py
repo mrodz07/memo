f30eacf52b3600eab2ed6214bbc733368487c7ed
Mario Rodríguez <mrodz_contacto@proton.me>
2024-05-19 18:59:20 -0600

    Refactored declension to include better error handling

--- memo/grammar/declension.py
+++ memo/grammar/declension.py
@@ -10,21 +10,46 @@ class DeclensionType(Enum):
 # TODO: Add irregular parameter for nouns that don't follow regular
 # terminations or don't have singular/plural number
 # TODO: Check if terminations are correct depending on DeclensionType
-class Declension():
+class Declension:
     cases = ["nominative", "genitive", "dative", "accusative", "ablative"]
+    dec = {}
+
     # sing for singular, plur for plural
     # dec for declensions
-    def __init__(self,
-                 dec_type: DeclensionType,
-                 sing_dec: dict[str, str], plur_dec: dict[str, str],
-                 ):
-        if not all([case in sing_dec for case in self.cases]) or \
-            not all([case in plur_dec for case in self.cases]):
-            raise Exception("Dictionary with incorrect keys passed to Declension")
-        else:
-            if all(sing_dec.values()) and all(plur_dec.values()):
-                self.sing_dec = sing_dec
-                self.plur_dec = plur_dec
-                self.dec_type = dec_type
-            else:
-                raise Exception("Emtpy value on dict passed to Declension")
+    def __init__(
+        self,
+        dec_type: DeclensionType,
+        sing_dec: dict[str, str],
+        plur_dec: dict[str, str],
+    ):
+        if (
+            self.check_dec_keys("Singular", sing_dec)
+            and self.check_dec_keys("Plural", plur_dec)
+            and self.check_no_empty_vals("Singular", sing_dec)
+            and self.check_no_empty_vals("Plural", plur_dec)
+        ):
+            self.sing_dec = sing_dec
+            self.plur_dec = plur_dec
+            self.dec_type = dec_type
+
+    # Check if the dec keys (cases) are correct.
+    # case_num refers to case number
+    def check_dec_keys(self, case_num, dec_dic):
+        if self.cases == len(dec_dic):
+            for case in dec_dic:
+                if case not in dec_dic.values():
+                    raise Exception(f"Invalid case on {case_num}: {case}")
+
+            return True
+        else:  # There are only 5 latin cases
+            raise Exception(
+                f"Incorrect dict length on {case_num}: Expected 4 got {len(dec_dic)}"
+            )
+
+    # case_num refers to case number
+    def check_no_empty_vals(self, case_num, dec_dic):
+        for key, val in dec_dic.values():
+            if val is None:
+                raise Exception(f"Empty value found on: {case_num}, {key}")
+
+        return True
