df71f997484b09ef2242a020efd9f626be2d5f80
Mario Rodríguez <mrodz_contacto@proton.me>
2024-05-19 18:27:09 -0600

    Rebasing tests/declension to dev

--- tests/test_declension.py
+++ tests/test_declension.py
@@ -3,7 +3,6 @@ from memo.grammar import declension
 from memo.grammar.declension import DeclensionType
 from memo.grammar.declension import Declension
 
-
 correct_declension_data = [
     DeclensionType(1),
     {
@@ -23,13 +22,53 @@ correct_declension_data = [
 ]
 
 
-def test_init_correct_dict():
+def test_init_passess_correct_dict():
     assert Declension(*correct_declension_data)
 
 
-def test_init_dicts_missing_key():
-    bad_declension_data = correct_declension_data
-    bad_declension_data[1].pop("nominative")  # Key removed
+class TestSingularDeclensions:
+    def test_init_fails_dict_missing_key(self):
+        bad_declension_data = correct_declension_data
+        del bad_declension_data[1]["nominative"]  # Key removed
 
-    with pytest.raises(Exception):
-        Declension(*bad_declension_data)
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
+
+    def test_init_fails_dict_wrong_key(self):
+        bad_declension_data = correct_declension_data
+        del bad_declension_data[1]["genitive"]  # Key removed
+        bad_declension_data[1]["not_valid"] = "not valid"
+
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
+
+    def test_init_fails_dict_empty_values(self):
+        bad_declension_data = correct_declension_data
+        bad_declension_data[1]["genitive"] = ""  # Value is empty
+
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
+
+
+class TestPlurarDeclensions:
+    def test_init_fails_dict_missing_key(self):
+        bad_declension_data = correct_declension_data
+        del bad_declension_data[2]["nominative"]  # Key removed
+
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
+
+    def test_init_fails_dict_wrong_key(self):
+        bad_declension_data = correct_declension_data
+        del bad_declension_data[2]["genitive"]  # Key removed
+        bad_declension_data[2]["not_valid"] = "not valid"
+
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
+
+    def test_init_fails_dict_empty_values(self):
+        bad_declension_data = correct_declension_data
+        bad_declension_data[2]["genitive"] = ""  # Value is empty
+
+        with pytest.raises(Exception):
+            Declension(*bad_declension_data)
