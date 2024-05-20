dec4c4cc95c10e34ea7aed48cf1c82e86db70b92
Mario Rodríguez <mrodz_contacto@proton.me>
2024-05-20 14:22:49 -0600

    Refactores tests/declension to be more readable

--- tests/test_declension.py
+++ tests/test_declension.py
@@ -1,74 +1,92 @@
 import pytest
-from memo.grammar import declension
+
 from memo.grammar.declension import DeclensionType
 from memo.grammar.declension import Declension
 
-correct_declension_data = [
-    DeclensionType(1),
-    {
+correct_dec_data = {
+    "dec_type": DeclensionType(1),
+    "sing": {
         "nominative": "porta",
         "genitive": "portae",
         "dative": "portae",
         "accusative": "portam",
         "ablative": "porta",
     },
-    {
+    "plur": {
         "nominative": "portae",
         "genitive": "portarum",
         "dative": "portis",
         "accusative": "portas",
         "ablative": "portis",
     },
-]
+}
 
 
 def test_init_passess_correct_dict():
-    assert Declension(*correct_declension_data)
+    assert Declension(
+        correct_dec_data["dec_type"], correct_dec_data["sing"], correct_dec_data["plur"]
+    )
 
 
 class TestSingularDeclensions:
+    num = "sing"
+
     def test_init_fails_dict_missing_key(self):
-        bad_declension_data = correct_declension_data
-        del bad_declension_data[1]["nominative"]  # Key removed
+        bad_dec_data = correct_dec_data
+        del bad_dec_data[self.num]["nominative"]  # Key removed
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
 
     def test_init_fails_dict_wrong_key(self):
-        bad_declension_data = correct_declension_data
-        del bad_declension_data[1]["genitive"]  # Key removed
-        bad_declension_data[1]["not_valid"] = "not valid"
+        bad_dec_data = correct_dec_data
+        del bad_dec_data[self.num]["genitive"]  # Key removed
+        bad_dec_data[self.num]["not_valid"] = "not valid"
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
 
     def test_init_fails_dict_empty_values(self):
-        bad_declension_data = correct_declension_data
-        bad_declension_data[1]["genitive"] = ""  # Value is empty
+        bad_dec_data = correct_dec_data
+        bad_dec_data[self.num]["genitive"] = ""  # Value is empty
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
 
 
 class TestPlurarDeclensions:
+    num = "plur"
+
     def test_init_fails_dict_missing_key(self):
-        bad_declension_data = correct_declension_data
-        del bad_declension_data[2]["nominative"]  # Key removed
+        bad_dec_data = correct_dec_data
+        del bad_dec_data[self.num]["nominative"]  # Key removed
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
 
     def test_init_fails_dict_wrong_key(self):
-        bad_declension_data = correct_declension_data
-        del bad_declension_data[2]["genitive"]  # Key removed
-        bad_declension_data[2]["not_valid"] = "not valid"
+        bad_dec_data = correct_dec_data
+        del bad_dec_data[self.num]["genitive"]  # Key removed
+        bad_dec_data[self.num]["not_valid"] = "not valid"
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
 
     def test_init_fails_dict_empty_values(self):
-        bad_declension_data = correct_declension_data
-        bad_declension_data[2]["genitive"] = ""  # Value is empty
+        bad_dec_data = correct_dec_data
+        bad_dec_data[self.num]["genitive"] = ""  # Value is empty
 
         with pytest.raises(Exception):
-            Declension(*bad_declension_data)
+            Declension(
+                bad_dec_data["dec_type"], bad_dec_data["sing"], bad_dec_data["plur"]
+            )
